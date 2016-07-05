a20b9febbcb5373c0f52dfaf37af9a40cb66cae3
ravenusmc <mcuddy77@gmail.com>
2016-07-05 11:14:33 -0400

    created a game loop function

--- game.py
+++ game.py
@@ -17,50 +17,54 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A Bit Racey')
 #Defining the game clock-for frames per second
 clock = pygame.time.Clock()
-
+#assing the car.bmp file to carImg
 carImg = pygame.image.load('car.bmp')
 
 #Function to make car show up on the screen. 
 def car(x,y):
   gameDisplay.blit(carImg,(x,y))
 
-x = (display_width * 0.30)
-y = (display_height * 0.6)
-#setting variables to change car location.
-x_change = 0
+def game_loop():
+  x = (display_width * 0.30)
+  y = (display_height * 0.6)
+  #setting variables to change car location.
+  x_change = 0
 
-#Setting a boolean flag to False
-crashed = False
+  #Setting a boolean flag to False
+  crashed = False
 
-while not crashed:
+  while not crashed:
 
-  #Gets any event that happens on the screen
-  for event in pygame.event.get():
-    #QUIT is from pgame
-    if event.type == pygame.QUIT:
-      crashed = True 
+    #Gets any event that happens on the screen
+    for event in pygame.event.get():
+      #QUIT is from pgame
+      if event.type == pygame.QUIT:
+        crashed = True 
 
-    #Adding ability to move car.
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        x_change = -5
-      elif event.key == pygame.K_RIGHT:
-        x_change = 5
+      #Adding ability to move car.
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+          x_change = -5
+        elif event.key == pygame.K_RIGHT:
+          x_change = 5
 
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-        x_change = 0
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+          x_change = 0
 
-  #Changing the location of the car.
-  x += x_change
+    #Changing the location of the car.
+    x += x_change
 
 
-  gameDisplay.fill(white)
-  car(x,y)
-  pygame.display.update()
-  #How many frames per second.
-  clock.tick(60)
+    gameDisplay.fill(white)
+    car(x,y)
+    pygame.display.update()
+    #How many frames per second.
+    clock.tick(60)
 
+
+
+game_loop()
 pygame.quit()
 quit()
 
